bd21ad82900162d519a281f5ee72c8e4f7dd07d5
Unknown <mgijon94@gmail.com>
2018-03-08 14:59:01 +0100

    Hola Mundo

---
+++ HolaMundo.java
@@ -0,0 +1,24 @@
+/* 
+	Hola Mundo en lenguaje java 
+    ===========================
+
+  OBS 1 : El nombre del archivo y el de la clase principal deben coincidir.
+  -------
+
+  OBS 2 : Solamente queremos que se muestre por pantalla el mensaje 'Hola Mundo', 
+  -------  para ello utilizaremos el método 'println' de la clase estática 
+           'System.out'
+
+  OBS 3: para compilar escribimos por consola 'javac HolaMundo.java', mientras que
+  -------  para ejecutar escribiremos 'java HolaMundo'
+*/
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola Mundo");
+	
+	}
+
+}
