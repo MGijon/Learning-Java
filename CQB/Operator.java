4e02b8edbd9bfc7aca40423da0f37d4e41c06242
Unknown <mgijon94@gmail.com>
2018-12-23 00:00:12 +0100

    Starting CQB simulator project

---
+++ CQB/Operator.java
@@ -0,0 +1,13 @@
+public class Operator{
+  /* Atributtes */
+  public double vision_angle = 120;
+  /* Methods */
+  public double get_vision_angle(){
+    /* return the atributte vision_angle */
+    return this.vision_angle;
+  }
+  public void set_vision_angle(double x){
+    /* set value for the parametter vision_angle */
+    this.vision_angle = x;
+  }
+}
