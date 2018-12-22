9029c37348dbd7ef19eef812742df40489663d19
Unknown <mgijon94@gmail.com>
2018-12-23 00:13:43 +0100

    Avanzando en 'Operador.java'  y 'hola mundo'

--- CQB/Operator.java
+++ CQB/Operator.java
@@ -1,12 +1,34 @@
 public class Operator{
-  /* Atributtes */
+  /* Constructor
+     =========== */
+  public Operator(String name, int team){
+    this.name = name;
+    this.team = team;
+  }
+
+  /* Atributtes
+    =========== */
   public double vision_angle = 120;
-  /* Methods */
-  public double get_vision_angle(){
+
+  /* Methods
+     ======= */
+  protected String getName(){
+    return this.name;
+  }
+  private void setName(String x){
+    this.name = x;
+  }
+  protected int getTeam(){
+    return this.team;
+  }
+  private void setTeam(int x){
+    this.team = x;
+  }
+  public double getVision_angle(){
     /* return the atributte vision_angle */
     return this.vision_angle;
   }
-  public void set_vision_angle(double x){
+  public void setVision_angle(double x){
     /* set value for the parametter vision_angle */
     this.vision_angle = x;
   }
---
+++ CQB/main.java
@@ -0,0 +1,10 @@
+/* tengo que añadir aquí el resot de clases */
+
+public class main{
+  static void main(String[] args){
+    System.out.println("Hello World");
+  }
+}
+
+/* javac main.java
+   java main */
