61adc89a1ad04071871b0a161a8515cdb4b60070
Unknown <mgijon94@gmail.com>
2018-12-24 18:06:10 +0100

    Principal en lugar de main

--- CQB/principal.java
+++ CQB/principal.java
@@ -1,7 +1,7 @@
 /* tengo que añadir aquí el resot de clases */
 
-public class main{
-  static void main(String[] args){
+public class principal{
+  public static void main(String[] args){
     System.out.println("Hello World");
   }
 }
