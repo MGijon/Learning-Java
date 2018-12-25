70317fe46f386f4c2e8068f4c22b112de32e06a6
Unknown <mgijon94@gmail.com>
2018-12-25 17:30:40 +0100

    Trabajando en la ventana principal

--- CQB/principal.java
+++ CQB/principal.java
@@ -6,44 +6,88 @@ import java.awt.event.*;
 public class principal{
 
   public static void main (String[] args){
-    JFrame frame = new JFrame("Test");
+
+    /* creamos la ventana con nombre 'Scenario' */
+    JFrame frame = new JFrame("Scenario");
     frame.setVisible(true);
-    frame.setSize(500,200);
+    frame.setSize(750,750);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    /* JPanel */
     JPanel panel = new JPanel();
     frame.add(panel);
-    JButton button = new JButton("hello agin1");
+
+    /* Botón 1 */
+    JButton button = new JButton("Add");
     panel.add(button);
     button.addActionListener (new Action1());
 
-    JButton button2 = new JButton("hello agin2");
+    /* Botón 2 */
+    JButton button2 = new JButton("Configuration");
     panel.add(button2);
     button.addActionListener (new Action2());
+
+    /* Botón 3 */
+    JButton button3 = new JButton("Play");
+    panel.add(button3);
+    button.addActionListener (new Action3());
   }
+
   static class Action1 implements ActionListener {
+
     public void actionPerformed (ActionEvent e) {
-      JFrame frame2 = new JFrame("Clicked");
+      /* crea una nueva ventana */
+      JFrame frame2 = new JFrame("Add");
       frame2.setVisible(true);
       frame2.setSize(200,200);
-      JLabel label = new JLabel("you clicked me");
+
+      /* Añadimos cosas en ella usando un panel y una etiqueta */
+      JLabel label = new JLabel("adding shit");
       JPanel panel = new JPanel();
+
       frame2.add(panel);
       panel.add(label);
     }
+
   }
+
   static class Action2 implements ActionListener {
+
     public void actionPerformed (ActionEvent e) {
-      JFrame frame3 = new JFrame("OKNO 3");
+      /* crea una nueva ventana */
+      JFrame frame3 = new JFrame("Configuration");
       frame3.setVisible(true);
       frame3.setSize(200,200);
 
-      JLabel label = new JLabel("kliknales");
+      /* Añadimos cosas en ella usando un panel y una etiqueta */
+      JLabel label = new JLabel("kabum kabum");
       JPanel panel = new JPanel();
+
       frame3.add(panel);
       panel.add(label);
     }
+
   }
+
+  static class Action3 implements ActionListener {
+
+    public void actionPerformed (ActionEvent e) {
+      /* crea una nueva ventana */
+      JFrame frame3 = new JFrame("Ventana 3");
+      frame3.setVisible(true);
+      frame3.setSize(200,200);
+
+      /* Añadimos cosas en ella usando un panel y una etiqueta */
+      JLabel label = new JLabel("bla bla bla bla");
+      JPanel panel = new JPanel();
+
+      frame3.add(panel);
+      panel.add(label);
+    }
+
+  }
+
+
 }
 
 /* javac main.java
